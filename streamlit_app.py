c6c2dca3f6e3881fd20566ef08a66df476a89116
Jambesh <jambesh@users.noreply.github.com>
2022-11-04 17:09:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("hello streamlit")
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
